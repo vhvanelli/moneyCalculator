63e5c918af11b7580358972b99f143e71b607813
Victor Hugo Faria Vanelli <vhvanelli@gmail.com>
2021-06-07 10:59:56 -0300

    I changed the 'if' to 'for' in the 'main.js'. Thus optimizing the code.

--- main.js
+++ main.js
@@ -3,27 +3,13 @@ function calcule(){
     var select = document.getElementById("select-coin");
     var value = select.options[select.selectedIndex].value;
     var result;
+    var coinValue = [{coinName:" Dólares", valueCoin:5.05}, {coinName:" Euros", valueCoin:6.14}, {coinName:" Libras", valueCoin:7.15} ]
     var coin;
-    var coinValue;
 
-    if(value == 1 & real > 0){
-       coinValue = 5.05;
-       coin = " Dólares"; 
-       result = (real / coinValue).toFixed(2);
+    for(coin = 0; coin != value; coin++){     
+        result = (real / coinValue[coin].valueCoin).toFixed(2);
     }
-    else if(value == 2 & real > 0){
-        coinValue = 6.14;
-        coin = " Euros"
-        result = (real / coinValue).toFixed(2);
-    }
-    else if(value == 3 & real > 0){
-        coinValue = 7.15;
-        coin = " Libras"
-        result = (real / coinValue).toFixed(2);
-    }
-    else{
-        alert("Coloque o valor em real ou escolha para qual moeda deseja converter!");
-        document.getElementById("value-id").innerHTML = "$"; 
-    }
-     document.getElementById("value-id").innerHTML = result + coin; 
+    coin--;
+    
+    document.getElementById("value-id").innerHTML = result + coinValue[coin].coinName;
 }   
